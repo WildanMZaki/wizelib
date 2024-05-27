7184a955efa71a9cdccf37f334ead5da4b9b8188
WildanMZaki <wildanmzaki7@gmail.com>
2024-05-27 08:45:25 +0700

    fix: json method

---
+++ logz.js
@@ -0,0 +1,24 @@
+const Logz = require("./lib/logz/Logz");
+
+const logz = new Logz();
+
+logz.info("This is info");
+logz.error("This is error");
+logz.debug("This is debug");
+logz.warning("This is warning");
+logz.docs("This is docs");
+logz.stackoverflow("This is stackoverflow");
+logz.sponsor("This is sponsor");
+logz.label("See");
+logz.label("With Json").json({ test: true });
+logz.error("Record error").record();
+logz.json("abc");
+logz
+  .json({
+    abc: "string",
+    iamnull: null,
+    iu: undefined,
+  })
+  .record();
+
+logz.now();
